e78f79fdbaf6fb96b04ae0e656502ea31fed5ca3
NgocHunter <91679248+NgocHunter@users.noreply.github.com>
2025-08-14 22:53:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kingbull-web/',
+  base: '/kingbull-growth-hub/',
   build: {
     outDir: 'docs',
   },
